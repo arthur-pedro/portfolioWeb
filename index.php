e1024b85097d11b50b77f6d7258cf46bb7b1271b
Arthur P L S Santos <arthurpedroweb@gmail.com>
2018-08-20 11:41:50 -0300

    Inicio - front em andamento

---
+++ index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Portfolio</title>
+	 <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+	
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+</head>
+<body>
+
+	<header>
+		<div class="container-fluid myNav">
+			<!-- <div class=" navbar-header"> -->
+				
+					<table class="table myTable">
+						<tr>
+							<th class="myList-header text-center"><a class="navbar-brand" href="#">PORTFOLIO</a></th>
+							<th class="myList-header text-center"><a href="#"><span class="fa fa-shopping-cart"></span></a></th>
+							<th id="nav-logar" class="myList-header text-center"><a href="#">LOGAR</a></th>
+
+						</tr>
+					</table>
+				
+		    		<!-- <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+			        <span class="fa fa-bars"></span> 
+				</button>
+		     	<h5><a  class=" navbar-brand" href="#">PORTFOLIO</a></h5>
+		    </div>
+		    <div class="collapse navbar-collapse" id="myNavbar">
+			    <ul class="nav navbar-nav navbar-right">
+			    	<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown"href="#"><h3><span class="fa fa-shopping-cart"></span></h3></a></a> -->
+				        
+				        <!-- <ul  class="dropdown-menu">
+				          <li class="dropdown-header"><a href="#">Mais popular</a></li>
+				          <li><a href="#"><span class="fa fa-star"></span>Ecomerce</a></li>
+				          <li role="presentation" class="divider"></li>
+				          <li class="dropdown-header"><a href="#">Todas as ategorias</a></li>
+				          <li><a href="#">Sistema a</a></li>
+				          <li><a href="#">Sistema b</a></li>
+				          <li><a href="#">Sistema c</a></li>
+				          <li><a href="#">Sistema d</a></li>
+				        </ul> -->
+			    <!-- 	</li>
+			    </ul> -->
+			<!-- </div> -->   
+		</div>
+		<div class="container-fluid myContainer">
+			<div class="row">
+				<table class="table myTable">
+					<tr>
+						<th class="myList text-center"><a href="#"><span class="fa fa-filter"></span> FILTRAR</a></th>
+						<th class="myList text-center"><a href="#"><span class="fa fa-star"></span>ECOMERCE</a></th>
+						<th class="myList text-center"><a href="#"><span class="caret"></span>CATEGORIA</a></th>
+						<th class="myList text-center"><a href="#">PORTFOLIO</a></th>
+						<th class="myList text-center"><a href="#">TECNOLOGIA</a></th>
+					</tr>
+				</table>
+			</div>	
+		</div>
+	</header>
+	
+	<section id="id-2">
+		<div class="container">
+				<div class="row">
+				<div class="col-md-8">
+					<br>
+					<p class="">BEST WEBSITES EXAMPLES OF PORTFOLIO DESIGNS <a href="#">read more</a></p>
+				</div><div class="col-md-4">
+					<br>
+					<p class="text-center">6 results for 'Portfolio websites'</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section>
+		<div class="container myContainer-2">
+			<div class="row">
+				
+				<div class="col-md-4">
+					<div class="box">
+						<div class="box-icon">
+                            <img src="assets/img/site-3.jpg" alt="">
+                        </div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="box">
+						<div class="box-icon">
+                            <img src="assets/img/site-2.png" alt="">
+                        </div>
+                    </div>
+				</div>
+				<div class="col-md-4">
+					<div class="box">
+						<div class="box-icon">
+                            <img src="assets/img/site-3.jpg" alt="">
+                        </div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				
+				<div class="col-md-4">
+					<div class="box">
+						<div class="box-icon">
+                            <img src="assets/img/site-3.jpg" alt="">
+                        </div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="box">
+						<div class="box-icon">
+                            <img src="assets/img/site-2.png" alt="">
+                        </div>
+                    </div>
+				</div>
+				<div class="col-md-4">
+					<div class="box">
+						<div class="box-icon">
+                            <img src="assets/img/site-3.jpg" alt="">
+                        </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section style="padding-top: 20px;">
+		<!-- <div class="container-fluid myContainer">
+			<div style="background-color: #40a085;">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-2"></div>
+						<div class="col-md-8">
+							<div class="box-header">
+								<h1 class="text-center">Gostou de algum?</h1>
+								<h3 class="text-center">Entre em contato nocosco!</h3>
+							</div>
+						</div>
+						<div class="col-md-2"></div>
+					</div>
+				</div>
+			</div>
+		</div> -->
+	</section>
+
+
+
+</body>
+</html>
